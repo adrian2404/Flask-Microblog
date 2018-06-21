b7472f5310ec885ae074074f86f40a67e36aed70
adrian2404 <adrian.yavorski@gmail.com>
2018-06-21 17:26:54 +0300

    Addressing PR comments: pep8 better flash message formatting more unusual secret-key :)

--- app/routes.py
+++ app/routes.py
@@ -25,7 +25,7 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username,
-                                                                   form.remember_me))
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data,
+                                                                   form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign in', form=form)
--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-duper-secret-key'
