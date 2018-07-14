ebd65be3957cec0f24ceabdd5623ed43dae1be28
adrian2404 <adrian.yavorski@gmail.com>
2018-07-14 17:10:11 +0300

    Users login using flask login app to make life easier create new view, route, template for user login

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +10,24 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign in')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    repeat_password = PasswordField('Repeat Password',
+                                    validators=[EqualTo('password'), DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('User with such username exists. '
+                                  'Please, choose a different username')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('User with such email exists.'
+                                  'Please, use a different email')
--- microblog.py
+++ microblog.py
@@ -7,4 +7,5 @@ def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
 
 
-app.run()
+if __name__ == "__main__":
+    app.run()
